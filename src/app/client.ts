c89889ac9657019166ecae11590636a17ce27961
Robert James <rjam@Roberts-MacBook-Air.local>
2022-10-28 18:04:55 -0500

    Auto-generated commit [ci skip]

--- src/app/client.ts
+++
@@ -1,9 +0,0 @@
-export class Client {
-  constructor(
-    public id: number,
-    public firstName: string,
-    public lastName: string,
-    public email: string,
-    public reason: string
-  ) {}
-}
